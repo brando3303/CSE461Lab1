73441bc5c914bc6fa6836b6d927388e1798f8bfb
solden <solden@uw.edu>
2025-10-09 20:00:08 -0700

    part b start

--- part1.py
+++ part1.py
@@ -26,20 +26,40 @@ def main(args):
     to_send = header + payload
     s.send(to_send)
 
-    data = s.recv(16)
-    unpacked = struct.unpack("!4I", data)
-    num, length, udp_port, secretA = unpacked
+    data = s.recv(28)
+    print(data[12:])
+    unpacked = struct.unpack("!4I", data[12:])
+    num_packets, length, udp_port, secretA = unpacked
+    print(unpacked)
 
     # Part b
-    addrinfo = socket.getaddrinfo(host, udp_port, socket.AF_INET)
-    for addr in addrinfo:
-        try:
-            s.connect(addr[4])
-            break
-        except socket.error as e:
-            continue
+    s.connect((host, udp_port))
+    s.settimeout(0.5)
     
+    total_bytes = length + (-1 * length % 4)
+    for i in range(num_packets):
+        payload = struct.pack("!I", i)
+        for _ in range(total_bytes):
+            payload += b"x\00"
+        header = get_header(len(payload), secretA, 1)
+        to_send = header + payload
+
+        # Send until we get ack from server
+        while True:
+            try:
+                s.send(to_send)
+                data = s.recv(4)
+                break
+            except socket.timeout as e:
+                continue
+
+        unpacked = struct.unpack("!I", data)
+        if (unpacked[0] != i):
+            print("uh oh")
     
+    data = s.recv(8)
+    unpacked = struct.unpack("!II", data)
+    print(unpacked)
     
 def get_header(payload_len, secret, step):
     return struct.pack('!IIHH', int(payload_len), int(secret), int(step), 758)
