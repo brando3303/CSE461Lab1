7efe8dd7f50f05e65334c82784cf8b64db6ea15f
solden <solden@uw.edu>
2025-10-21 23:50:23 -0700

    updated server

--- part2.py
+++ part2.py
@@ -17,8 +17,9 @@ BANNED_PORT = 41201
 PARTA_HEADER = (12, 0, 1, )
 PARTA_PAYLOAD = b"hello world\0"
 
-# Event to handle closing sockets on each thread when server is stopped
-SHUTDOWN_EVENT = threading.Event()
+# Global list of active sockets for cleanup
+sockets = []
+sockets_lock = threading.Lock()
 
 def main(args):
     if (len(args) != 3):
@@ -49,6 +50,9 @@ def server(host, port):
 
     # Start with a UDP port that the clients will connect to
     listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    with sockets_lock:
+        sockets.append(listener)
+
     listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listener.bind(('', port))
 
@@ -63,121 +67,209 @@ def server(host, port):
             # start. Spawn a thread to handle individual clients on separate servers
             # later down the line.
 
-
             if addr not in processed_clients:
                 processed_clients.append(addr)
                 thread = threading.Thread(target=server_loop, args=(listener, addr, data), daemon=True)
                 thread.start()
     except KeyboardInterrupt:
-        SHUTDOWN_EVENT.set()
+        print()
         print("Server terminated by user")
         sys.exit()
+    finally:
+        with sockets_lock:
+            for s in sockets:
+                s.close()
+        print("All sockets closed")
         
 
 def server_loop(listener, addr, data):
     # Run the logic of the server loop
     num, udp_port, secretA, len1 = part_a(listener, data, addr)
-    part_b(num, udp_port, secretA, len1)
+    tcp_port, secretB = part_b(num, udp_port, secretA, len1)
+    num2, len2, c, secretC, tcp_sock = part_c(tcp_port, secretB) 
+    part_d(num2, len2, c, secretC, tcp_sock)
     print(f"Finished connection from {addr}")
     return
 
 def part_a(listener, data, addr):
     # Part a
-    if DEBUG:
-        print(f"Recieved {data} from {addr}")
-
-    if len(data) != 24:
-        return
-        # listener.close() dont need to close the udp socket because the server is listening
-
-    # Check header against expected, close listner if it's wrong
-    payload = validate_packet(data, 12, 0, 1, None)
-    if not payload:
+    try:
         if DEBUG:
-            print("Header check failed")
-        # listener.close()
-        return
+            print(f"Recieved {data} from {addr}")
 
-    payload = data[12:]
-    print(payload)
+        if len(data) != 24:
+            return
+            # listener.close() dont need to close the udp socket because the server is listening
 
-    if payload != PARTA_PAYLOAD:
+        # Check header against expected, close listner if it's wrong
+        payload = validate_packet(data, 12, 0, 1, None)
+        if not payload:
+            if DEBUG:
+                print("Header check failed")
+            # listener.close()
+            return
+
+        payload = data[12:]
+        print(payload)
+
+        if payload != PARTA_PAYLOAD:
+            if DEBUG:
+                print("Payload check failed")
+            # listener.close()
+            return
+
+        #generate response
+        num = random.randint(1, 100)
+        len1 = random.randint(1, 128)
+
+        # TODO
+        udp_port = random.randint(MIN_PORT, MAX_PORT)  # check that this is a valid port to bind to
+        secretA = random.randint(1, 4096)
+
+        response_header = generate_header(16, 0, 2)
+        response_payload = struct.pack("!IIII", num, len1, udp_port, secretA)
+        print(response_payload)
+        to_send = response_header + response_payload
+        listener.sendto(to_send, addr)
+
+        return (num, udp_port, secretA, len1)
+    except OSError as e:
         if DEBUG:
-            print("Payload check failed")
-        # listener.close()
-        return
+            print(f"OSError: {e}. Thread shutting down.")
+            sys.exit()
 
-    #generate response
-    num = random.randint(1, 100)
-    len1 =   random.randint(1, 128)
-    udp_port = random.randint(MIN_PORT, MAX_PORT)  # check that this is a valid port to bind to
-    secretA = random.randint(1, 4096)
-
-    response_header = generate_header(16, 0, 2)
-    response_payload = struct.pack("!IIII", num, len1, udp_port, secretA)
-    print(response_payload)
-    to_send = response_header + response_payload
-    listener.sendto(to_send, addr)
-
-    return (num, udp_port, secretA, len1)
-
-def part_b( num, udp_port, secretA, len1):
+def part_b(num, udp_port, secretA, len1):
     # Part b
-    print(f"Part B beginning...")
-    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_sock.settimeout(1)
-    udp_sock.bind(('', udp_port)) # need to error handle this
-    for i in range(num):
-        data, addr = udp_sock.recvfrom(1024)  # wait for packet
-        packet = validate_packet(data, len1 + 4, secretA, 1, None)
-        if not packet:
-            if DEBUG:
-                print("Packet validation failed")
-            udp_sock.close()
-            return
-        #check packet_id
-        packet_id = struct.unpack("!I", packet[:4])[0]
-        if packet_id != i:
-            if DEBUG:
-                print(f"Packet ID mismatch: expected {i}, got {packet_id}")
-            udp_sock.close()
-            return
-        # Send ack
-        ack_header = generate_header(4, secretA, 2)
-        ack_payload = struct.pack("!I", i)
-        udp_sock.sendto(ack_header + ack_payload, addr)
+    try:
+        print(f"Part B beginning...")
+        udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    tcp_port = random.randint(MIN_PORT, MAX_PORT)
-    secretB = random.randint(1, 4096)
-    payload = struct.pack("!II", tcp_port, secretB)
-    header = generate_header(8, secretA, 2)
-    udp_sock.sendto(header + payload, addr)
-    udp_sock.close()
-    print("Closing connection")
-    return
+        # Add sockets to global list
+        with sockets_lock:
+            sockets.append(udp_sock)
 
-def part_c():
+        udp_sock.settimeout(1)
+        udp_sock.bind(('', udp_port)) # need to error handle this
+        for i in range(num):
+            data, addr = udp_sock.recvfrom(1024)  # wait for packet
+            packet = validate_packet(data, len1 + 4, secretA, 1, None)
+            if not packet:
+                if DEBUG:
+                    print("Packet validation failed")
+
+                # Remove socket from global list
+                with sockets_lock:
+                    sockets.remove(udp_sock)
+                
+                udp_sock.close()
+
+                sys.exit()
+            #check packet_id
+            packet_id = struct.unpack("!I", packet[:4])[0]
+            if packet_id != i:
+                if DEBUG:
+                    print(f"Packet ID mismatch: expected {i}, got {packet_id}")
+                with sockets_lock:
+                    sockets.remove(udp_sock)
+                udp_sock.close()
+                sys.exit()
+            # Send ack
+            ack_header = generate_header(4, secretA, 2)
+            ack_payload = struct.pack("!I", i)
+            udp_sock.sendto(ack_header + ack_payload, addr)
+
+        tcp_port = random.randint(MIN_PORT, MAX_PORT)
+        secretB = random.randint(1, 4096)
+        payload = struct.pack("!II", tcp_port, secretB)
+        header = generate_header(8, secretA, 2)
+        udp_sock.sendto(header + payload, addr)
+        with sockets_lock:
+            sockets.remove(udp_sock)
+        udp_sock.close()
+        print("Closing connection")
+        return tcp_port, secretB
+    except OSError as e:
+        if DEBUG:
+            print(f"OSError: {e}. Thread shutting down.")
+            sys.exit()
+
+def part_c(tcp_port, secretB):
     # Part c
-    return
+    try:
+        print("Part C beginning...")
+        tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_sock.settimeout(3)
+        with sockets_lock:
+            sockets.append(tcp_sock)
+
+        # Bind and listen for client
+        tcp_sock.bind(('', tcp_port))
+        tcp_sock.listen(1)
+        conn, addr = tcp_sock.accept()
+        tcp_sock.settimeout(None)
+
+        if DEBUG:
+            print(f"Connected to {addr}")
+        
+        # Generate random ints and send to client
+        num2 = random.randint(1,16)
+        len2 = random.randint(1,16)
+        secretC = random.randint(1, 4096)
+        c = chr(random.randint(95,122))
+        payload = struct.pack("!III", num2, len2, secretC)
+        print(payload)
+        payload += bytes(str(c), 'utf-8') + b"\0\0\0"
+        header = generate_header(13, secretB, 2)
+        conn.send(header + payload)
+        
+        return num2, len2, c, secretC, tcp_sock
+    except socket.timeout as e:
+        print(f"Socket timed out: {e}")
+        with sockets_lock:
+            sockets.remove(tcp_sock)
+
+        tcp_sock.close()
+        sys.exit()
+    except OSError as e:
+        if DEBUG:
+            print(f"OSError: {e}. Thread shutting down.")
+            sys.exit()
+
 
 def part_d(num2, len2, c, secretC, sock):
     # Part d
-    data = sock.recv(1024)
-    payload = validate_packet(data, len2, secretC, 4, None)
-    if not payload:
-        if DEBUG:
-            print("Packet validation failed")
+    try:
+        print("Part D beginning...")
+        data = sock.recv(1024)
+        payload = validate_packet(data, len2, secretC, 4, None)
+        if not payload:
+            if DEBUG:
+                print("Packet validation failed")
+            with sockets_lock:
+                sockets.remove(sock)
+            sock.close()
+        expected_payload = c.encode() * num2
+        if payload != expected_payload:
+            if DEBUG:
+                print("Payload mismatch")
+            with sockets_lock:
+                sockets.remove(sock)
+            sock.close()
+        header = generate_header(0, secretC, 5)
+        payload = struct.pack("!I", random.randint(1, 100))
+        sock.send(header + payload)
+        print("Closing connection")
+        with sockets_lock:
+            sockets.remove(sock)
+
         sock.close()
-    expected_payload = c.encode() * num2
-    if payload != expected_payload:
+        return
+    
+    except OSError as e:
         if DEBUG:
-            print("Payload mismatch")
-        sock.close()
-    header = generate_header(0, secretC, 5)
-    payload = struct.pack("!I", random.randint(1, 100))
-    sock.send(header + payload)
-    print("Closing connection")
-    return
+            print(f"OSError: {e}. Thread shutting down.")
+            sys.exit()
+
 
 def check_header(header, expected, parta=False):
     """
